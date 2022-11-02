41a8751408fbfe8ea69602eed6b7912d504f766f
Cesar Baquiro <91351156+CesarBaquiro@users.noreply.github.com>
2022-11-02 12:18:02 -0600

    Primera modificacion
    Se realizo una breve modificacion la cual redujo las iteraciones de  211 a 141

--- tamiz.js
+++ tamiz.js
@@ -1,13 +1,13 @@
 /*En este ejercicio vamos a mostrar todos los numeros numeros primos que se encuentren en un rango dinamico */
 
-let numero = 100; //Rango de numeros a evaluar
+let numero = 120; //Rango de numeros a evaluar
 let primos = []; //Arreglo de numeros primos (int)
 let esPrimo = []; //Arreglo de numeros primos(Bool)
 let iteracion = 0; //Controlador de iteraciones
 
 if (numero >= 2) {
   //Comprobamos que el numero ingresado sea mayor a 2
-  for (let i = 0; i <= numero; i++) {
+  for (let x = 0; x <= numero; x++) {
     esPrimo.push(true); //Llenamos todo el arreglo esPrimo con true
   }
 
@@ -15,14 +15,20 @@ if (numero >= 2) {
     //Aca llenamos el arreglo primos con true
     if (esPrimo[i]) {
       //Si el numero en la posicion i del arreglo esPrimo es true
-      primos.push(i); //Guardamos la posicion del true en el arreglo primos
+      primos.push(i); //2, 3
 
-      for (let j = 1; j * i <= numero; j++) {
-        esPrimo[i * j] = false; //Si la posicion
-        iteracion++; //Contador para el numero de iteraciones
+      if (i < 8) {
+        if (i != 4) {
+          for (let j = 1; j * i <= numero; j++) {
+            esPrimo[i * j] = false;
+
+            iteracion++; //Contador para el numero de iteraciones
+          }
+        }
       }
     }
   }
+
   console.log(primos);
   console.log("El numero de las iteraciones es " + iteracion);
 } else {
